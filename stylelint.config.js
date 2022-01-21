34c6dd2425e715b621531b489c9733f593deee52
wangjunwei <wangjunwei@nucarf.com>
2022-01-21 15:31:21 +0800

    add: add post-html package

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   root: true,
   plugins: ['stylelint-order'],
+  customSyntax: 'postcss-html',
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   rules: {
     'selector-pseudo-class-no-unknown': [
