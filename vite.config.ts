a09aee73fbcef5d6bde3b9041bcc7b0e33592421
Gavin <gavinwjw@163.com>
2023-06-16 16:41:05 +0800

    ci: update proxy target

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://123.56.76.249:3000',
         changeOrigin: true
       }
     }
