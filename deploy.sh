82aff349291c3bec997de016e91f7dfe5b1149d2
Gavin <gavinwjw@163.com>
2023-06-26 14:59:02 +0800

    chore: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,4 +20,4 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-git push -f git@github.com:wjw-gavin/vue3-element-plus-docs.git master:gh-pages
+git push -f git@github.com:wjw-gavin/vue3-vite-ts-element-plus.git main:gh-pages
