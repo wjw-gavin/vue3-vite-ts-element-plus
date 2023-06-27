1d574683414cd3c3fba76ec990c92fe13742ac32
Gavin <gavinwjw@163.com>
2023-06-27 13:52:57 +0800

    docs: update docs

---
+++ assets/pages_guide_start.md.b42d9514.js
@@ -0,0 +1,13 @@
+import{_ as s,o as a,c as n,O as l}from"./chunks/framework.6664f35c.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"pages/guide/start.md","filePath":"pages/guide/start.md","lastUpdated":1687845105000}'),e={name:"pages/guide/start.md"},p=l(`<div class="language-bash"><button title="Copy Code" class="copy"></button><span class="lang">bash</span><pre class="shiki material-theme-palenight"><code><span class="line"><span style="color:#676E95;font-style:italic;"># 下载</span></span>
+<span class="line"><span style="color:#FFCB6B;">git</span><span style="color:#A6ACCD;"> </span><span style="color:#C3E88D;">clone</span><span style="color:#A6ACCD;"> </span><span style="color:#C3E88D;">git@github.com:wjw-gavin/vue3-vite-ts-element-plus.git</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#82AAFF;">cd</span><span style="color:#A6ACCD;"> </span><span style="color:#C3E88D;">vue3-vite-ts-element-plus</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"># 安装依赖</span></span>
+<span class="line"><span style="color:#FFCB6B;">pnpm</span><span style="color:#A6ACCD;"> </span><span style="color:#C3E88D;">install</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"># 本地运行</span></span>
+<span class="line"><span style="color:#FFCB6B;">pnpm</span><span style="color:#A6ACCD;"> </span><span style="color:#C3E88D;">dev</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"># 打包</span></span>
+<span class="line"><span style="color:#FFCB6B;">pnpm</span><span style="color:#A6ACCD;"> </span><span style="color:#C3E88D;">build</span></span></code></pre></div>`,1),t=[p];function o(c,i,r,y,_,d){return a(),n("div",null,t)}const u=s(e,[["render",o]]);export{m as __pageData,u as default};
