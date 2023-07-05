6d5cf626ba864895b20bfff4f55f6450adbbf97b
Gavin <gavinwjw@163.com>
2023-07-05 10:55:50 +0800

    chore: update proxy target

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
+        // target: 'http://vue-study.wangjunwei.vip',
         changeOrigin: true
       }
     }
